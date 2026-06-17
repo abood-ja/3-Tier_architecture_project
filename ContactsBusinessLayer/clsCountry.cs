8501490232169c8877b4201324f7e7130f67ea1b
abood.jarrar <abd.a.jarrar@gmail.com>
2026-06-17 21:17:23 +0300

    finalyze implementing the functions for the country table in the business logic layer

---
+++ ContactsBusinessLayer/clsCountry.cs
@@ -0,0 +1,111 @@
+﻿using ContactsDataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactsBusinessLayer
+{
+    public class clsCountry
+    {
+        public enum enMode { AddNew = 0, Update = 1 };
+        public enMode Mode = enMode.AddNew;
+        public int CountryID { set; get; }
+        public string CountryName { set; get; }
+        public clsCountry() {
+            this.CountryID = -1;
+            this.CountryName = "";
+            Mode = enMode.AddNew; ;
+        }
+
+        private clsCountry(int CountryID, string CountryName)
+        {
+            this.CountryID= CountryID;
+            this.CountryName=CountryName;
+            Mode = enMode.Update;
+        }
+
+        public static clsCountry FindCountryByID(int CountryID)
+        {
+            string CountryName = "";
+            if (clsContactDataAccess.GetCountryInfoByID(CountryID,ref CountryName))
+            {
+                return new clsCountry(CountryID, CountryName);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public static clsCountry FindCountryByName(string CountryName)
+        {
+            int CountryID = -1;
+            if (clsContactDataAccess.GetCountryInfoByName(ref CountryID,  CountryName))
+            {
+                return new clsCountry(CountryID, CountryName);
+            }
+            else
+            {
+                return null;
+            }
+        }
+        private bool _AddNewCountry()
+        {
+            this.CountryID = clsContactDataAccess.AddNewCountry(this.CountryName);
+            return this.CountryID != -1;
+        }
+
+        private bool _UpdateCountry()
+        {
+
+            return clsContactDataAccess.UpdateCountry(this.CountryID,this.CountryName);
+        }
+
+
+        public bool Save()
+        {
+            switch (Mode)
+            {
+                case enMode.AddNew:
+                    if (_AddNewCountry())
+                    {
+                        Mode = enMode.Update;
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                case enMode.Update:
+                    return _UpdateCountry();
+            }
+
+
+            return false;
+
+        }
+
+        public static bool DeleteCountry(int CountryID)
+        {
+            return clsContactDataAccess.DeleteCountry(CountryID);
+        }
+
+        public static DataTable GetAllCountries()
+        {
+            return clsContactDataAccess.GetAllCountries();
+        }
+
+        public static bool IsCountryExistByID(int CountryID)
+        {
+            return clsContactDataAccess.IsCountryExistByID(CountryID);
+        }
+
+        public static bool IsCountryExistByName(string CountryName)
+        {
+            return clsContactDataAccess.IsCountryExistByName(CountryName);
+        }
+    }
+}
